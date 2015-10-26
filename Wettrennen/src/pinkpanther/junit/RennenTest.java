902e903736baf40ce8f03497f64a1b4a0c112c34
Marcel Suter <marcel.suter@bzz.ch>
2015-10-26 15:38:29 +0100

    update

---
+++ Wettrennen/src/pinkpanther/junit/RennenTest.java
@@ -0,0 +1,31 @@
+package pinkpanther.junit;
+
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+import pinkpanther.rennen.Rennen;
+import pinkpanther.tier.Panther;
+
+public class RennenTest {
+    Rennen rennen = new Rennen("Cooles Rennen", 4, new Vector<Panther>(), 1000);
+
+    @Test
+    public void testForErmittleGewinner() {
+        String[] pantherName = { "Pink Panther", "Red Panther", "Blue Panther",
+                "Black Panther" };
+
+        for (int i = 0; i < 4; i++) {
+            rennen.addPanther(new Panther(pantherName[i]));
+        }
+        rennen.getPanthers().get(0)
+                .setZurueckgelegterWeg(rennen.getZurueckzulegenderWeg());
+        rennen.getPanthers().get(1)
+                .setZurueckgelegterWeg(rennen.getZurueckzulegenderWeg() + 100);
+        assertEquals(rennen.ermittleGewinner(rennen.getPanthers()), rennen
+                .getPanthers().get(1));
+    }
+
+}
--- Wettrennen/src/pinkpanther/rennen/RennenErzeuger.java
+++ Wettrennen/src/pinkpanther/rennen/RennenErzeuger.java
@@ -9,14 +9,13 @@ public class RennenErzeuger {
     public static void main(String[] args) {
         String[] pantherName = { "Pink Panther", "Red Panther", "Blue Panther",
                 "Black Panther" };
-        Rennen rennen = new Rennen("Cooles Rennen", 5,
-                new Vector<Panther>(), 1000);
-        for (int i = 0; i < 4; i++) {
+        Rennen rennen = new Rennen("Cooles Rennen", 4, new Vector<Panther>(),
+                1000);
+        for (int i = 0; i < rennen.getAnzTeilnehmer(); i++) {
             rennen.addPanther(new Panther(pantherName[i]));
         }
-        
         rennen.durchfuehren(rennen);
-        for(Panther p : rennen.getPanthers()){
+        for (Panther p : rennen.getPanthers()) {
             System.out.println(p.toString());
         }
     }
